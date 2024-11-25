8bf825644361026b1cd5a251fd42abc672f5108c
joao.gueiros <joao.gueiros@allcomtelecom.com>
2024-11-25 15:51:51 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains('light')) {
+    //se tiver light mode, add a img light
+    img.setAttribute('src', './assets/avatar_light.png')
+  } else {
+    //se tiver sem light mode, manter a imagem norm
+    img.setAttribute('scr', './assets/avatar.png')
+  }
+
+  if (html.classList.contains("")) {
+    //se tiver light mode, add a img light
+    img.setAttribute("src", "./assets/avatar.png")
+  } else {
+    //se tiver sem light mode, manter a imagem norm
+    img.setAttribute("scr", "./assets/avatar_light.png")
+  }
+}
+
